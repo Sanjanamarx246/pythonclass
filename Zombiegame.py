c16036dca41ef243ebd2eeb782fe929eff959647
Sanjana <student2@planetrobotics.com>
2023-03-28 20:06:14 -0400

    Hey me

--- Zombiegame.py
+++ Zombiegame.py
@@ -3,3 +3,11 @@ print("Hey you! yeah the one reading this.\n \
       but it is in a high security palace and you need to get it.\n \
     what will you do? answer and you shall know")
 
+user = input("Enter '1. to fight the men and leave without the gold or enter 2. to take the risk \n \
+ and take a sneaky right to the room full of salty water?'")
+if user == '1':
+    print("YOU LOSE!!!! try again!")
+elif user == '2':
+    print("Great now you in a room full of salty water what do you do next?")
+user = input("Enter '1.you go in a door and you escape or 2. to take the risk \n \
+ and go into the basement and won't know if you will drown?'")
