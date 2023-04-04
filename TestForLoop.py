e1bc09b4ffa6f707a68d3646142e8d2dba495e36
Sanjana <student2@planetrobotics.com>
2023-04-04 18:32:08 -0400

    hey me

---
+++ TestForLoop.py
@@ -0,0 +1,26 @@
+# create list of numbers
+# print each number in that list
+
+numList= [2, 4, 6, 8, 10, 12, 14]
+for number in numList:
+    print(number)
+    print(number * number)
+
+
+
+
+# add all the numbers in the list
+addition = 0
+for number in numList:
+    addition= addition + number
+
+print(addition)
+
+multiply = 1
+for number in numList:
+    multiply = multiply * number
+
+print(multiply)
+
+
+
