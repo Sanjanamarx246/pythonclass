3c86696554246e43a50e94cfd218167713610f08
Sanjana <student2@planetrobotics.com>
2023-05-02 20:06:15 -0400

    hey me

--- Passwordcheckerapp.py
+++ Passwordcheckerapp.py
@@ -2,9 +2,9 @@ import customtkinter as ctk
 def enter_clicked():
     print("enter clicked")
     name=namEntry.get()
-    Address = AddressEntry.get()
+    Password = PasswordEntry.get()
     print(name)
-    print(Address)
+    print(Password)
     infolabel.configure(text=name)
     strList = [name, Address]
     info='\n'.join(strList)
@@ -18,14 +18,16 @@ app.title("Password Checker")
 nameLabel = ctk.CTkLabel(app, text="Name: ")
 nameLabel.grid(row=0, column=0)
 
+
 namEntry = ctk.CTkEntry(app, placeholder_text="Enter your name")
 namEntry.grid(row=0, column=1)
 
-addressLabel = ctk.CTkLabel(app, text="password: ")
-addressLabel.grid(row=1, column=0)
 
-AddressEntry = ctk.CTkEntry(app, placeholder_text="Enter Password")
-AddressEntry.grid(row=1, column=1)
+PasswordLabel = ctk.CTkLabel(app, text="password: ")
+PasswordLabel.grid(row=1, column=0)
+
+PasswordEntry = ctk.CTkEntry(app, placeholder_text="Enter Password")
+PasswordEntry.grid(row=1, column=1)
 
 enterbutton = ctk.CTkButton(app, text="Enter", command= enter_clicked)
 enterbutton.grid(row=2, column=1)
